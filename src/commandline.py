86a38c71451b547c14b8130dc933a5939eb70a0f
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-11-19 17:04:13 +0900

    update a commandline
    add the time measurement

--- src/commandline.py
+++ src/commandline.py
@@ -1,7 +1,34 @@
+from functools import wraps
+import time
 import setting as conf
 import ksdft
 
-nuclear_numbers, geom_coordinates, basis_set_name, ksdft_functional_name = conf.get_calc_params()
-calc_mol = ksdft.driver(nuclear_numbers, geom_coordinates,
-                   basis_set_name, ksdft_functional_name)
-calc_mol.ks_scf()
+def stop_watch(func):
+  """ Measure time """
+  @wraps(func)
+  def wrapper(*args, **kargs):
+    start = time.time()
+
+    result = func(*args, **kargs)
+
+    elapsed_time = time.time() - start
+
+    print("")
+    print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+    with open('elapsed_time.dat', 'w') as tfile:
+      tfile.write("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+    return result
+  return wrapper
+
+@stop_watch
+def run_ksdft():
+  nuclear_numbers, geom_coordinates, basis_set_name, ksdft_functional_name = conf.get_calc_params()
+  calc_mol = ksdft.driver(nuclear_numbers, geom_coordinates,
+                    basis_set_name, ksdft_functional_name)
+  calc_mol.ks_scf()
+
+
+if __name__ == "__main__":
+  run_ksdft()
