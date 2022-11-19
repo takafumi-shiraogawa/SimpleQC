dab0cf520bfafb525d2f3ba752afe3080a8625fd
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-11-19 16:54:30 +0900

    add a driver of KS-DFT

---
+++ src/ksdft.py
@@ -0,0 +1,24 @@
+import psi4
+import interface_psi4 as ipsi4
+
+
+class driver():
+  def __init__(self, nuclear_numbers, geom_coordinates, basis_set_name, ksdft_functional_name):
+    self._nuclear_numbers = nuclear_numbers
+    self._geom_coordinates = geom_coordinates
+    self._basis_set_name = basis_set_name
+    self._ksdft_functional_name = ksdft_functional_name
+
+
+  def ks_scf(self):
+    # Not direct SCF
+
+    proc_ao_integral = ipsi4.proc_psi4(
+        self._nuclear_numbers, self._geom_coordinates,
+        self._basis_set_name, self._ksdft_functional_name)
+
+    # Compute all requisite integrations
+    ao_kinetic_integral = proc_ao_integral.ao_kinetic_integral()
+    ao_hartree_potential_integral = proc_ao_integral.ao_hartree_potential_integral()
+    ap_electron_repulsion_integral = proc_ao_integral.ap_electron_repulsion_integral()
+    ao_overlap_integral = proc_ao_integral.ao_overlap_integral()
